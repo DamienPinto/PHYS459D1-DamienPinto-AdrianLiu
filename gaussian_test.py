b1934b1c340f69347d5114aef384b3730d338ffb
DamienPinto <31781186+DamienPinto@users.noreply.github.com>
2018-09-09 10:05:49 -0400

    Update gaussian_test.py
    Cleaned up a bit.

--- gaussian_test.py
+++ gaussian_test.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.mlab import bivariate_normal
 from mpl_toolkits.mplot3d import Axes3D
 
-universe = np.zeros((80, 80, 100))
+universe = np.zeros((80, 80, 101))
 
 #Parameters to set
 mu_x = 10
@@ -12,8 +12,6 @@ sigma_x = np.sqrt(5)
 mu_y = 10
 sigma_y = np.sqrt(5)
 
-fig = plt.figure()
-ax = fig.add_subplot(1, 2, 1, projection='3d')
 
 #Create grid and multivariate normal
 x = np.linspace(0,20,80)
@@ -23,32 +21,29 @@ Z = bivariate_normal(X,Y,sigma_x,sigma_y,mu_x,mu_y)
 
 for i in range(80):
 	for j in range(80):
-		for k in range(int(Z[i, j]/np.amax(Z)*100)):
-			
-			universe[i][j][k] = 1
+
+		universe[i][j][int(Z[i, j]/np.amax(Z)*100)] = 1
 print 'Done.'
 
-# x2 = np.linspace(-1, 1, 500)
-# y2 = np.linspace(-1, 1, 500)
-# X2, Y2 = np.meshgrid(x, y)
 
-fourier_universe = np.fft.fftshift(np.fft.rfftn(universe, axes=(0, 1)))/np.sqrt(2*6400)
-# #Make a 3D plot
-# surf1 = ax.plot_surface(X, Y, Z,cmap='viridis',linewidth=0)
-# ax.set_xlabel('X1 axis')
-# ax.set_ylabel('Y1 axis')
-# ax.set_zlabel('Z1 axis')
+fourier_universe = np.fft.ax.plot_surface(X, Y, Z,cmap='viridis',linewidth=0)
 
 z, x, y = universe.nonzero()
+
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 2, 1, projection='3d')
 ax.scatter(z, x, y, zdir = 'z', c = 'red')
+ax.set_xlabel('X1 axis')
+ax.set_ylabel('Y1 axis')
+ax.set_zlabel('Z1 axis')
 
 
 z, x, y = fourier_universe.nonzero()
-# print fourier_universe
 print np.amax(fourier_universe)
 
 
-# ax = fig.add_subplot(1, 2, 2, projection='3d')
+ax = fig.add_subplot(1, 2, 2, projection='3d')
 surf2 = ax.scatter(z, x, y, zdir='z' , c = 'red')
 ax.set_xlabel('X2 axis')
 ax.set_ylabel('Y2 axis')
